69cff40089cf18f525dadcf1aae3fd0777a8d630
anes89cok <anes89cok@gmail.com>
2015-04-01 14:42:56 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="9225 Gulfshore DR. N. , FL 34108"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="FL"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2395926557;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
